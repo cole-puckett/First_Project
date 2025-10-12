ed849db769c9ac823806f85752e448a84e3c84da
Cole Puckett <cjp7e@mtmail.mtsu.edu>
2025-10-12 17:24:43 -0500

    Update GradeCalculator.cpp

--- GradeCalculator.cpp
+++ GradeCalculator.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 #include <print>
+#include <string>
 
 int main (){
 
+int displayaverage(double);
 
+  
+int grade{};
+std::println("Enter a grade to add to your average: ");
+std::cin >> grade;
+  // repeatedly ask for another grade until the stop input 
+double total{};
+total = grade; 
+int count{1};
+  
+while ( grade != "Stop" ) {
+  std::string next_grade{};
+  std::println("Enter next grade (or \"Stop\" to stop): ");
+  std::cin >> next_grade{};
+  ++count;
+
+if ( next_grade == "Stop"){
+displayaverage();
+}
+  
+  total += next_grade;
+  
+}
 
 
 return 0;
+}
+
+int displayaverage(double total){
+  double average{};
+  average = total / count;
+  std::println("Your grade average is: {}", average);
+
+  
 }
