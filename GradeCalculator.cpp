8c00457c4650b44e926e62593946b6439ec22c59
Cole Puckett <cjp7e@mtmail.mtsu.edu>
2025-10-13 04:15:11 +0000

    Changes to be committed: 	new file:   GradeAverage 	modified:   GradeCalculator.cpp 	modified:   README.md 	new file:   a.out

--- GradeCalculator.cpp
+++ GradeCalculator.cpp
@@ -1,42 +1,85 @@
 #include <iostream>
-#include <print>
 #include <string>
+#include <iomanip>
 
 int main (){
-
-int displayaverage(double);
-
+  
+  void display_average(double, int);
+  int validate_grade(double);
+  void letter_grade(double);
   
 double grade{};
-std::println("Enter a grade to add to your average: ");
+std::cout << "Enter a grade to add to your average: ";
 std::cin >> grade;
+validate_grade(grade);
   // repeatedly ask for another grade until the stop input 
 double total{};
 total = grade; 
 int count{1};
-  
-while ( grade != "Stop" ) {
-  std::string user_input{};
-  std::println("Enter next grade (or \"Stop\" to stop): ");
-  std::cin >> user_input{};
-  ++count;
+std::string user_input{};
 
-if ( user_input == "Stop"){
-displayaverage();
-} else {
-  double next_grade = std::stod(user_input); 
-  total += next_grade;
+double next_grade{};
+
+while ( user_input != "Stop" && user_input != "stop") {
+  std::cout << "Enter next grade (or \"Stop\" to stop): ";
+  std::cin >> user_input;
+  ++count;
+  
+  if ( user_input != "Stop" && user_input != "stop"){
+    
+    next_grade = std::stod(user_input); 
+    
+    validate_grade(next_grade);
+    total += next_grade;
+    
+  }  
 }
-}
+
+display_average(total, count);
 
 
 return 0;
 }
+void letter_grade(double final_grade){
 
-int displayaverage(double total){
+  if (final_grade >= 90){
+    std::cout << "You got an A! Amazing work!\n";
+  } else if (final_grade >=80){
+    std::cout << "You got a B! Brilliant effort\n";
+  } else if ( final_grade >= 70){
+    std::cout << "You got a C! Creative stuff!\n";
+  } else if ( final_grade >= 60){
+    std::cout << "You got a D... You're still passing!!\n";
+  } else {
+    std::cout << "You got an F... there's always next time\n";
+  }
+
+  return;
+}
+
+void display_average(double total, int count){
+ 
+  std::setprecision(2);
+ std::fixed;
   double average{};
   average = total / count;
-  std::println("Your grade average is: {}", average);
+  std::cout << "Your grade average is: " << average << "\n" ;
 
-  
+  letter_grade(average);
+
+  return;
+}
+
+int validate_grade(double grade){
+
+if ( grade < 0 || grade > 100){
+      if (grade < 0 ){
+        std::cout << "Invalid input: Grade can not be negative\n";
+        exit(1);
+     } else {
+        std::cout << "Invalid input: Grade can not exceed 100\n";
+        exit(1);
+     }
+   }
+   return grade;
 }
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # First_Project
-Testing for fun
+
+This is a simple C++ program that calculates the average of the grades entered by the user. This is my first personal project.
+
+## Features
+- Prompts user for a grade 
+- Handles invalid inputs (inputs outside of the 0 - 100 range)
+- Calculates the average and outputs it as well as the corresponding letter grade 
+
+## Future Improvements
+- Output median, highest, and lowest grades
+- Save and load grades from a text file
+- Handle invalid inputs (non-numerical and not the stop command)
+
+## What I learned
+- Converting between a string and numeric values to allow for a stop command as a word
+- Validating inputs from the user
+- Organizing code into functions for cleaner and more clear structure
+
+## How to Run
+Compile and run using any C++ compiler:
+g++ GradeCalculator.cpp -o grade_calculator
+
+./grade_calculator
